31c4341b0d1769c0428895f7d6c8f63869df2577
Andrey Masterenko J <amasterenko@gmail.com>
2021-02-08 13:01:10 +0300

    3. Immutable объекты [#466498]

---
+++ src/main/java/ru/job4j/concurrent/linked/ExtNode.java
@@ -0,0 +1,12 @@
+package ru.job4j.concurrent.linked;
+
+public class ExtNode<T> extends Node<T> {
+
+    public ExtNode(Node<T> next, T value) {
+        super(next, value);
+    }
+
+    public void set (T value) {
+
+    }
+}

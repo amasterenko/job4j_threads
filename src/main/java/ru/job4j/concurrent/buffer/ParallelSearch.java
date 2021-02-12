7fe3c6031819960b1a067bed6277678eb8a4907c
Andrey Masterenko H <masterenko.as@gmail.com>
2021-02-12 08:08:17 +0300

    2. Обеспечить остановку потребителя. [#466484]

---
+++ src/main/java/ru/job4j/concurrent/buffer/ParallelSearch.java
@@ -0,0 +1,42 @@
+package ru.job4j.concurrent.buffer;
+
+import ru.job4j.concurrent.wait.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    static int stop = -1;
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        try {
+                            int value = queue.poll();
+                            if (value == stop) {
+                                break;
+                            }
+                            System.out.println(value);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    try {
+                    for (int index = 0; index != 3; index++) {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                    }
+                    queue.offer(stop);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+        ).start();
+    }
+}
